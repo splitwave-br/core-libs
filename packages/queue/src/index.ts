3b4a2bfa7578181abd060c99fad466e26fbeb5a6
André Gaspar <andregaspar@splitwavegateway.com>
2025-04-04 15:31:56 -0300

    add adjusts on package.json

--- packages/queue/src/index.ts
+++ packages/queue/src/index.ts
@@ -1,3 +1,3 @@
-export * from './src/queue.module';
-export * from './src/queue.processor';
-export * from './src/queue.service';
+export * from './queue.module';
+export * from './queue.processor';
+export * from './queue.service';
