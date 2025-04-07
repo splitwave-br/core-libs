3bc9525d3695d1319a30730f6334c2e50e9314f8
André Gaspar <andregaspar@splitwavegateway.com>
2025-04-07 12:15:27 -0300

    comment script on core

--- packages/core/scripts/generate-barrel.ts
+++ packages/core/scripts/generate-barrel.ts
@@ -1,35 +1,35 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
+// import * as fs from 'fs';
+// import * as path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const srcDir = path.resolve(__dirname, '../src');
-const indexPath = path.join(srcDir, 'index.ts');
+// const srcDir = path.resolve(__dirname, '../src');
+// const indexPath = path.join(srcDir, 'index.ts');
 
-function collectExports(dir: string): string[] {
-  const entries = fs.readdirSync(dir, { withFileTypes: true });
-  const exports: string[] = [];
+// function collectExports(dir: string): string[] {
+//   const entries = fs.readdirSync(dir, { withFileTypes: true });
+//   const exports: string[] = [];
 
-  for (const entry of entries) {
-    if (entry.name === 'index.ts') continue;
+//   for (const entry of entries) {
+//     if (entry.name === 'index.ts') continue;
 
-    const fullPath = path.join(dir, entry.name);
-    const relativePath = path.relative(srcDir, fullPath).replace(/\\/g, '/');
+//     const fullPath = path.join(dir, entry.name);
+//     const relativePath = path.relative(srcDir, fullPath).replace(/\\/g, '/');
 
-    if (entry.isDirectory()) {
-      exports.push(...collectExports(fullPath));
-    } else if (entry.name.endsWith('.ts')) {
-      const withoutExt = relativePath.replace(/\.ts$/, '');
-      exports.push(`export * from './${withoutExt}';`);
-    }
-  }
+//     if (entry.isDirectory()) {
+//       exports.push(...collectExports(fullPath));
+//     } else if (entry.name.endsWith('.ts')) {
+//       const withoutExt = relativePath.replace(/\.ts$/, '');
+//       exports.push(`export * from './${withoutExt}';`);
+//     }
+//   }
 
-  return exports;
-}
+//   return exports;
+// }
 
-const exports = collectExports(srcDir);
-fs.writeFileSync(indexPath, exports.join('\n') + '\n');
+// const exports = collectExports(srcDir);
+// fs.writeFileSync(indexPath, exports.join('\n') + '\n');
 
-console.log(`✅ Gerado index.ts com ${exports.length} exports.`);
+// console.log(`✅ Gerado index.ts com ${exports.length} exports.`);
